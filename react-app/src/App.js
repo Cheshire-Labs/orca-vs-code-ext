1b014b857bfee787e958d3da1ac90a3bc74f0b38
Michael Salmi <miikee713@gmail.com>
2024-02-02 21:56:38 -0500

    save before changing extension.ts

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useRef } from 'react';
+import { dia, shapes } from 'jointjs';
 
 function App() {
+  const graphContainerRef = useRef(null);
+
+  useEffect(() => {
+    const graph = new dia.Graph();
+    const paper = new dia.Paper({
+      el: graphContainerRef.current,
+      model: graph,
+      width: 600,
+      height: 400,
+      gridSize: 10,
+    });
+
+    const rect = new shapes.standard.Rectangle();
+
+    rect.position(100, 30);
+    rect.resize(100, 40);
+    rect.attr({
+      body: {
+        fill: 'blue',
+      },
+      label: {
+        text: 'Hello JointJS',
+        fill: 'white',
+      },
+    });
+    graph.addCell(rect);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+      <div ref={graphContainerRef} style={{ width: '600px', height: '400px' }}></div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +49,7 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { OrcaTreeDataProvider } from './orca_treeview_provider';
+import * as path from 'path';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -20,11 +20,45 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from orca-gui in a web extension host!');
 	});
-
 	context.subscriptions.push(disposable);
+	
+	context.subscriptions.push(vscode.commands.registerCommand('orca-gui.Webview', () => {
+		const indexPath = path.join(context.extensionPath, 'build', 'index.html');
+		console.log('indexPath', indexPath);
+		const panel = vscode.window.createWebviewPanel(
+			'reactWebview', // Identifies the type of the webview. Used internally
+			'React Webview', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{
+				// Enable scripts in the webview
+				enableScripts: true,
 
-	const orcaTreeDataProvider = new OrcaTreeDataProvider();
-	context.subscriptions.push(vscode.window.createTreeView('orca-gui.view', { treeDataProvider: orcaTreeDataProvider }));
+				// Restrict the webview to only loading content from our extension's `build` directory.
+				localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'build'))]
+			}
+		);
+
+		// panel.webview.onDidReceiveMessage(message => {
+		// 	if (message.command === 'getHtml') {
+		// 		const indexPath = path.join(context.extensionPath, 'build', 'index.html');
+		// 		const indexUri = vscode.Uri.file(indexPath);
+		// 		vscode.workspace.openTextDocument(indexUri).then(document => {
+		// 			panel.webview.html = document.getText();
+		// 		});
+		// 	}
+		// });
+
+		// panel.webview.postMessage({ command: 'getHtml' });
+
+
+		// Read and display the HTML content at startup
+		console.log('indexPath', indexPath);
+		const indexUri = vscode.Uri.file(indexPath);
+		vscode.workspace.openTextDocument(indexUri).then(document => {
+			panel.webview.html = document.getText();
+		});
+	}));
+	
 }
 
 // This method is called when your extension is deactivated
